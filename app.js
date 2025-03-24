4db8e66c5a6c46d8769da55858cef8907cb8fe06
Rameez <rameesmohd789@gmail.com>
2025-03-24 15:58:01 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ const corsOptions = {
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(cors({ origin: '*' })); 
-app.use(express.json()); 
-app.use(bodyParser.urlencoded({ extended: true, limit: '6mb' }))
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true })); 
 app.use(bodyParser.json()); 
 require('./cronjobs/cronRollover')
 
