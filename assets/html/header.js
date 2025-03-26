1314fb84ae1aa823fc6d01633dd322045e9139e4
Rameez <rameesmohd789@gmail.com>
2025-03-26 13:12:33 +0530

    modified:   assets/html/header.js

--- assets/html/header.js
+++ assets/html/header.js
@@ -1,77 +1,71 @@
 const header = () => `
-              <table
-                cellspacing="0"
-                cellpadding="0"
-                align="center"
-                style="
-                  border-collapse: collapse;
-                  border-spacing: 0px;
-                  table-layout: fixed !important;
-                  width: 100%;
-                  background-color: transparent;
-                  background-repeat: repeat;
-                  background-position: center top;
-                "
-              >
-                <tbody>
-                  <tr>
-                    <td align="center" style="padding: 0; margin: 0">
-                      <table
-                      cellspacing="0"
-                      cellpadding="0"
-                      bgcolor="#ffffff"
-                      align="center"
-                      style="
-                        border-collapse: collapse;
-                        border-spacing: 0px;
-                        background-color:rgb(0, 0, 0) !important;;
-                        width: 600px;
-                      "
-                    >
+  <table
+    cellspacing="0"
+    cellpadding="0"
+    align="center"
+    style="
+      border-collapse: collapse;
+      border-spacing: 0px;
+      table-layout: fixed !important;
+      width: 100%;
+      background-color: transparent;
+      background-repeat: repeat;
+      background-position: center top;
+    "
+  >
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 0; margin: 0">
+          <table
+            cellspacing="0"
+            cellpadding="0"
+            bgcolor="#ffffff"
+            align="center"
+            style="
+              border-collapse: collapse;
+              border-spacing: 0px;
+              background-color: rgb(0, 0, 0) !important;
+              width: 100%;
+              max-width: 600px;
+            "
+          >
+            <tbody>
+              <tr>
+                <td
+                  align="left"
+                  bgcolor="#000000"
+                  style="padding-left: 0; margin: 0; background-color: #000000 !important;"
+                >
+                  <table width="100%" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border-spacing: 0px;">
                     <tbody>
                       <tr>
-                        <td
-                          align="left"
-                          bgcolor="#000000"
-                          style="
-                            padding-left: 0;
-                            margin: 0;
-                           
-                            background-color: #000000 !important;;
-                          "
-                        >
-<table width="100%" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border-spacing: 0px;">
-  <tbody>
-    <tr>
-      <td valign="top" align="center" style="padding: 0; margin: 0; width: 600px;">
-        <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="border-collapse: collapse; border-spacing: 0px;">
-          <tbody>
-            <tr>
-              <td align="center" style="padding: 0; margin: 0; font-size: 0px;">
-                <img
-                  src="https://res.cloudinary.com/dj5inosqh/image/upload/v1742816157/p2gjrfqeuvbjw1o9ba6q.png"
-                  alt=""
-                  style="display: block; border: 0; outline: none; text-decoration: none; margin: auto;" 
-                  height="100"
-                  class="CToWUd"
-                  data-bit="iit"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-</table>
-
+                        <td valign="top" align="center" style="padding: 0; margin: 0; width: 100%;">
+                          <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="border-collapse: collapse; border-spacing: 0px;">
+                            <tbody>
+                              <tr>
+                                <td align="center" style="padding: 0; margin: 0; font-size: 0px;">
+                                  <img
+                                    src="https://res.cloudinary.com/dj5inosqh/image/upload/v1742974888/x6phsdgc2towetj9efdy.png"
+                                    alt=""
+                                    style="display: block; border: 0; outline: none; text-decoration: none; margin: auto; width: 100%; max-width: 600px; height: auto;"
+                                    class="CToWUd"
+                                    data-bit="iit"
+                                  />
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
                         </td>
                       </tr>
                     </tbody>
-                    </table>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 `;
 module.exports = header;
